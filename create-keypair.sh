eef0de86b50445b0c9bcb688ffb951f9afcf51c8
student <marco.scarlino@students-live.uniroma2.it>
2022-09-20 04:00:02 -0700

    create, import and show scripts

---
+++ create-keypair.sh
@@ -0,0 +1,3 @@
+ssh-keygen -m PEM
+echo 'add the .pem extension to the private key file'
+echo 'move both in the ~/keypair directory'
---
+++ import-key.sh
@@ -0,0 +1,1 @@
+aws ec2 import-key-pair --key-name "SDCC" --public-key-material fileb://~/keypair/SDCC.pub
